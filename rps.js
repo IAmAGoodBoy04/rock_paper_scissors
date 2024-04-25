75d78b8912e09734b1c0418e0ffcb7c46bbd9a1e
Shubhan <shubhan.singh@gmail.com>
2024-04-25 23:12:03 +0530

    Add GUI in new branch

--- rps.js
+++ rps.js
@@ -8,13 +8,13 @@ function getComputerChoice(){
 
 function convertToText(number){
     if(number===0){
-        return "Rock";
+        return "rock";
     }
     else if(number === 1){
-        return "Paper";
+        return "paper";
     }
     else{
-        return "Scissors";
+        return "scissors";
     }
 }
 
@@ -42,7 +42,7 @@ function playRound(playerSelection, computerChoice){
     }
     if(playerSelection===computerChoice){
         return {
-            reply:"Computer chose "+convertToText(computerChoice)+" too,"+"Draw",
+            reply:`You both chose ${convertToText(computerChoice)}, Draw`,
             score:0.5,
         };
     }
@@ -59,25 +59,104 @@ function playRound(playerSelection, computerChoice){
         };
     }
 }
+let times_played=0;
+let sc_player=0;
+let sc_comp=0;
 
-function playGame(){
-    let playerSelection = convertToNumber(prompt("Enter your choice : ").trim().toLowerCase());
+let pmaincont=document.querySelector(".player.main_container");
+let cmaincont=document.querySelector(".computer.main_container");
+
+let bboxPlayer=document.querySelector(".big_box.player");
+let bboxComp=document.querySelector(".big_box.computer");
+bboxPlayer.innerHTML="<p>Please select one</p>";
+bboxComp.innerHTML="<p>Waiting for you</p>";
+let scp=document.querySelector(".score.player");
+let scc=document.querySelector(".score.computer");
+let re=document.querySelector(".result");
+scp.textContent=`Score : ${sc_player}`;
+scc.textContent=`Score : ${sc_comp}`;
+
+let option=document.querySelectorAll(".options.player>img")
+option.forEach((opt)=>{
+    opt.addEventListener("click",function optionclick(){
+        if(opt.classList.contains("Rock")){
+            bboxPlayer.innerHTML='<img src="./resources/SVGs/rock.svg" height="200px" width="auto"></img>';
+            playGame(0);
+        }
+        else if(opt.classList.contains("Paper")){
+            bboxPlayer.innerHTML='<img src="./resources/SVGs/paper.svg" height="200px" width="auto"></img>';
+            playGame(1);
+        }
+        else{
+            bboxPlayer.innerHTML='<img src="./resources/SVGs/scissors.svg" height="200px" width="auto"></img>';
+            playGame(2);
+        }
+    })
+})
+
+document.body.addEventListener("keypress",(e)=>{
+    if(e.key==="Enter"){
+        cleanup();
+    }
+});
+
+function playGame(playerSelection){
     let computerChoice=getComputerChoice();
-    alert("\nThe computer chose : "+convertToText(computerChoice)+"\n");
+    re.textContent="The computer chose : "+convertToText(computerChoice)+"\r\n";
+    bboxComp.innerHTML=`<img src="./resources/SVGs/${convertToText(computerChoice)}.svg" height="200px" width="auto"></img>`;
     result = playRound(playerSelection,computerChoice);
-    alert(result.reply);
-    return result.score;
+    sc_player+=result.score;
+    sc_comp+=(1-result.score);
+    scp.textContent=`Score : ${sc_player}`;
+    scc.textContent=`Score : ${sc_comp}`;
+    re.textContent=re.textContent+result.reply;
+    if(result.score==1){
+        pmaincont.classList.remove("lose");
+        cmaincont.classList.remove("win");
+        pmaincont.classList.add("win");
+        cmaincont.classList.add("lose");
+    }
+    else if(result.score==0.5){
+        pmaincont.classList.remove("lose");
+        cmaincont.classList.remove("win");
+        pmaincont.classList.remove("win");
+        cmaincont.classList.remove("lose");
+    }
+    else{
+        pmaincont.classList.remove("win");
+        cmaincont.classList.remove("lose");
+        pmaincont.classList.add("lose");
+        cmaincont.classList.add("win");
+    }
+    times_played++;
+    if(times_played===5){
+        if(sc_player===2.5){
+            re.textContent+=`\r\n\r\nYou drew with the computer 2.5-2.5`;
+            re.classList.add("draw");
+        }
+        else if(sc_player>2.5){
+            re.textContent+=`\r\n\r\nYou won against the computer ${sc_player}-${sc_comp}`;
+            re.classList.add("win");
+        }
+        else{
+            re.textContent+=`\r\n\r\nYou lost against the computer ${sc_player}-${sc_comp}`;
+            re.classList.add("lose");
+        }
+        re.textContent+="\r\nReload page to play again";
+        document.body.replaceWith(document.body.cloneNode(true));//to remove eventlisteners
+    }
+    else{
+        re.textContent+="\r\n\r\nPress Enter to reset GUI"
+    }
 }
-let score=0;
-for(i=0;i<5;i++){
-    score+=playGame();
+
+function cleanup(){
+    re.textContent="";
+    pmaincont.classList.remove("lose");
+    cmaincont.classList.remove("win");
+    pmaincont.classList.remove("win");
+    cmaincont.classList.remove("lose");
+    bboxPlayer.innerHTML="<p>Please select one</p>";
+    bboxComp.innerHTML="<p>Waiting for you</p>";
 }
-if(score==0){
-    alert(`You drew with the computer 2.5-2.5`);
-}
-else if(score>2.5){
-    alert(`You won against the computer ${score}-${5-score}`);
-}
-else{
-    alert(`You lost against the computer ${score}-${5-score}`);
-}
+
